d55677c24c32105ea7ff2dd1a2ac8f1f1a0960d9
emilgulayev <emilgotlib@gmail.com>
2023-08-16 21:32:01 +0300

    basic pokemon express API 2

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ app.use(express.json());
 app.use(cors());
 const port = 4000
 let db = require('./pokemon_db.json')
-db=db.map(pokemon=>({...pokemon,img:`https://img.pokemondb.net/sprites/silver/normal/${pokemon.name.toLowerCase()}.png`}))
+db=db.filter(pokemon=>/^[a-zA-Z]+$/.test(pokemon.name)).map(pokemon=>({...pokemon,img:`https://img.pokemondb.net/sprites/silver/normal/${pokemon.name.toLowerCase()}.png`}))
 
 
 app.get('/pokemon', (req, res) => {
-    const limit=req.query.limit || 5
-    const offset=req.query.offset || 0
-    const response = db.slice(offset,limit+1)
+    const limit=Number(req.query.limit) || 5
+    const offset=Number(req.query.offset) || 0
+    const sort= req.query.sort || 'asc'
+    const response = {data:db.sort((a,b)=>sort==='asc'?a.number-b.number:b.number-a.number).slice(offset,offset+ limit),total:db.length}
     res.send(response)
 })
 
